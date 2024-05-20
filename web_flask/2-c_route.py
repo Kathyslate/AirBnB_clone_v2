1a7a4f10076fd49bb387b7d88bc2b655162211cb
Kathyslate <mercyoyetunde98@gmail.com>
2024-05-20 01:55:52 -0700

    gygyghgh

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -16,6 +16,7 @@ def hbnb():
     """returns HBNB"""
     return 'HBNB'
 
+
 @app.route('/c/<text>', strict_slashes=False)
 def cisfun(text):
     """display “C ” followed by the value of the text"""
